0c5d0285426bb84b0dbbc21c5db3a425aee7228e
jun shi <junshinju@gmail.com>
2016-11-13 12:56:38 +0800

    添加注释

--- pinyin/hmm/viterbi2.py
+++ pinyin/hmm/viterbi2.py
@@ -1,6 +1,6 @@
 # -*- coding=utf8 -*-
 """
-    viterbi算法实现
+    viterbi算法实现,不使用sqllite,直接读取json存储的概率转移矩阵
 """
 from pinyin.model import Emission, Transition
 from json2sqlite.json2sqlite import json_load
@@ -9,7 +9,7 @@ def prepare_mat():
     start_mat = json_load("base_start.json")
     transition_mat = json_load("base_transition.json")
     emission_mat = json_load("base_emission.json")
-    pinyin_mat = {}
+    pinyin_mat = {}                      #计算拼音对应的汉字
     for hanzi in emission_mat.keys():
         for pinyin in emission_mat[hanzi]:
             if pinyin_mat.__contains__(pinyin):
@@ -27,15 +27,12 @@ def viterbi(pinyin_list):
         pinyin_list (list): 拼音列表
     """
     start_mat, transition_mat, emission_mat, pinyin_mat = prepare_mat()
-    start_char = {}
-    # start_char = Emission.join_starting(pinyin_list[0])
+    start_char = {}        #计算第一个拼音对应的汉字
     for hanzi in pinyin_mat[pinyin_list[0]].keys():
-        # print "hanzi", hanzi
         if start_mat.__contains__(hanzi):
             start_char[hanzi] = start_mat[hanzi] + emission_mat[hanzi][pinyin_list[0]]
 
-    V = {char: start_char[char] for char in start_char}
-
+    V = start_char
     for i in range(1, len(pinyin_list)):
         pinyin = pinyin_list[i]
         prob_map = {}
@@ -44,7 +41,7 @@ def viterbi(pinyin_list):
             maxProb = 0
             state = ""
             new_prob = 0
-            for hanzi in pinyin_mat[pinyin].keys():
+            for hanzi in pinyin_mat[pinyin].keys():    
                 if transition_mat.__contains__(character) \
                     and transition_mat[character].__contains__(hanzi):
                     tmpProb = transition_mat[character][hanzi] + emission_mat[hanzi][pinyin]
@@ -52,9 +49,7 @@ def viterbi(pinyin_list):
                         maxProb = tmpProb
                         state = hanzi
                         new_prob = tmpProb
-            # result = Transition.join_emission(pinyin, character)
             if not maxProb:
-                # del prob_map[phrase]
                 continue
             prob_map[phrase + state] = new_prob + prob
 
